030bcd93311062fa6a0ad9a97d675dfdf895c7d8
drew <me@andrinoff.com>
2025-08-14 00:16:59 +0400

    fix: edge runtime

--- src/app/api/tasks/route.ts
+++ src/app/api/tasks/route.ts
@@ -4,6 +4,7 @@ import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
 export const dynamic = "force-dynamic";
+export const runtime = "edge";
 
 export async function GET(request: Request) {
   const cookieStore = await cookies();
