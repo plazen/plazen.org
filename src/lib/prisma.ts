f2d41942d50c78e20feda2fb9e3d35a8076b09c3
drew <me@andrinoff.com>
2025-12-01 18:36:31 +0400

    chore(deps): update prisma to v7

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,20 @@
-import { PrismaClient } from "../generated/prisma";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@/generated/prisma/client";
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
-const prisma = global.prisma || new PrismaClient();
+const createPrismaClient = () => {
+  const connectionString = process.env.DATABASE_URL;
+  const pool = new Pool({ connectionString });
+  const adapter = new PrismaPg(pool);
+
+  return new PrismaClient({ adapter });
+};
+
+const prisma = global.prisma || createPrismaClient();
 
 if (process.env.NODE_ENV === "development") {
   global.prisma = prisma;
